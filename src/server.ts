a844a3fb193cc68e3cf2a957026f27d7b7345d65
gabrielSanovis <9novis9@gmail.com>
2024-09-28 23:09:19 -0300

    add port

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import humidityRouter from "./router/HumidityHistoricRoute";
+import { config } from "dotenv-safe";
+config();
 
 const app = express();
 
@@ -7,8 +9,7 @@ app.use(express.json());
 
 app.use(humidityRouter);
 
-const port = 3000;
-const IPv4 = "YOUR_IPV4"
-app.listen(port, IPv4, () => {
-    console.log(`Listening to requests on http://${IPv4}:${port}`);
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Listening to requests on PORT ${port}`);
 });
